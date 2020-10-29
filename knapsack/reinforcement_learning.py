f939832c5032db3505e0bbf991c1e5a8314aac5e
Bryce Johnson <joh14192@umn.edu>
2020-10-29 11:26:22 -0500

    comments to figure out where oracle should go

--- knapsack/reinforcement_learning.py
+++ knapsack/reinforcement_learning.py
@@ -1,6 +1,8 @@
 
 
 from tensorforce.execution import Runner
+from tensorforce.agents import ActorCritic
+from tensorforce.agents import AdvantageActorCritic
 
 
 #initialize environment using .make() or-gym. Environment.create wraps custom
@@ -36,25 +38,57 @@ class RL():
         episode_terminal = list()
         episode_reward = list()
 
+
+        # at start of an environment start at a random state.
+        # start an monte carlo random walk by starting at a random state.
         states = self.env.reset()
+
+        # i don't know what internals do yet...
         internals = self.agent.initial_internals()
+
+        # our environment class will tell us when we want to stop
         terminal = False
         while not terminal:
+            # this is a MONTE CARLO RANDOM WALK..
+            # this walk is dependent on the condition of
+            # terminal which we specified by the environment.
+            # the terminal condition is specified by the custom environment class
+            # in the execute function.
             episode_states.append(states)
             episode_internals.append(internals)
+
+            # here is where the agent acts, this says which action should i take?
+            # which action is optimal for me to take.
             actions, internals = self.agent.act(
                 states=states, internals=internals, independent=True
             )
             episode_actions.append(actions)
+
+
+            # here is where the environment executes... in custom environment we will
+            # have to define this function.
             states, terminal, reward = self.env.execute(actions=actions)
+
+            # if the reward look similar from previous steps , and is BAD,
+            # in this monte carlo random walk, we know we are stuck
+            # and need to call the oracle.
+
+            # todo: CALL ORACLE HERE.  but still need to update internals somehow. b/c
+            #  we will have one more state than internal.
+            #  so probabably have to call states, internals again...
+            
             episode_terminal.append(terminal)
             episode_reward.append(reward)
 
+
+        # update the state of the agent based on what it just learned in the previous episode.
         self.agent.experience(
             states=episode_states, internals=episode_internals,
             actions=episode_actions, terminal=episode_terminal,
             reward=episode_reward
         )
+
+        # update the agent.
         self.agent.update()
 
     def test(self):
