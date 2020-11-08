bd001c79ed16314c4ebc490b4324d5dca565d071
Bryce Johnson <joh14192@umn.edu>
2020-11-08 10:44:34 -0600

    comments for parameter optimization

--- bryce/test.py
+++ bryce/test.py
@@ -48,8 +48,16 @@ def main():
     n_actions = env.action_space.n
     actor = Actor(state_dim, n_actions)
     critic = Critic(state_dim)
+
+
+    # training hyperparameters right here.
+    # here we are constructing an optimizer object
     adam_actor = torch.optim.Adam(actor.parameters(), lr=1e-3)
     adam_critic = torch.optim.Adam(critic.parameters(), lr=1e-3)
+
+    # future reward discount... hyperparameter tuned in some way .
+    # future reward discount: sums up value of all future states.
+    # [0,1] how greedy are you going to be.
     gamma = 0.99
 
     episode_rewards = []
@@ -59,21 +67,44 @@ def main():
         done = False
         total_reward = 0
         state = env.reset()
-
+        # monte carlo random walk
+        # done :boolean a terminal condition.
         while not done:
+            # find the probabilities
             probs = actor(t(state))
+
+            # make a categorical ditribution from probailities
             dist = torch.distributions.Categorical(probs=probs)
+
+            # randomly sample from that. distribution is built on prior information.
             action = dist.sample()
 
+            # environment takes its next step. , actor critic stuff.
             next_state, reward, done, info = env.step(action.detach().data.numpy())
+
+            # COULD BE: part of tuning system ... but we dont know yet.
+            # gamma being closer to zero means more greedy.
+
+            # so accounting for future state more. #todo: come back to this
             advantage = reward + (1 - done) * gamma * critic(t(next_state)) - critic(t(state))
+
 
             total_reward += reward
             state = next_state
 
-            critic_loss = advantage.pow(2).mean()
+
+            # critic loss ... mean(a^2)
+            # we are optimizing the hyperparameters of the nn's
+
+
+            # todo: look at the optimization parameters.
+            critic_loss = advantage.pow(2).mean() # why are they doing a mean call here?
             adam_critic.zero_grad()
+
+            # back propagation
             critic_loss.backward()
+
+            # Performs a single optimization step (parameter update).
             adam_critic.step()
 
             actor_loss = -dist.log_prob(action) * advantage.detach()
